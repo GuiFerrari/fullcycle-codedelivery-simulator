b3e4dc0df456db1d38ae275ce07507fdf1813107
Guilherme Ferrari <guiferrari10@hotmail.com>
2021-04-08 11:19:58 -0300

    Created GO Project

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	kafka2 "github.com/guiferrari/codedelivery-simulator/application/kafka"
+	"github.com/guiferrari/codedelivery-simulator/infra/kafka"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+
+	go consumer.Consume()
+
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
+}
